d0a986a9fd8e9c3976d34b6670ebf7c9e23472b8
J1n-Haerosa <mulyati4425@gmail.com>
2026-06-05 18:50:09 +0700

    fix syntax

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 import os
 import json
 import time
@@ -932,6 +931,3 @@ if __name__ == '__main__':
     print("===========================================")
     
     serve(app, host='0.0.0.0', port=5000, threads=12)
-
-
-```
